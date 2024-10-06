000f8e057f5b3a4a1cf38874e59a35b4e30b0359
Haletran <bpasquier123@gmail.com>
2024-10-07 00:51:17 +0200

    add: better styling and add login screen (commented for now)

--- dev_game/src/js/app.js
+++ dev_game/src/js/app.js
@@ -27,16 +27,3 @@ back_button.addEventListener("click", function () {
     canvas.style.setProperty("display", "none", "important");
   });
 });
-
-// https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_typewriter
-let i = 0;
-let txt = "Ft_transcendence";
-let speed = 50;
-
-window.onload = function typeWriter() {
-  if (i < txt.length) {
-    document.getElementById("game_name2").innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
-  }
-};
